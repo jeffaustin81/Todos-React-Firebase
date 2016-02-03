f88ab826c1dd36614bf41f66670a98664f8a27be
Jeff Austin <jeffaustin81@gmail.com>
2016-02-02 18:04:51 -0800

    Add list.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,12 +2,21 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var Header = require('./header');
+var List = require('./list');
 var rootUrl = 'https://dazzling-inferno-9857.firebaseio.com/';
 
 var Hello = React.createClass({
   mixins: [ ReactFire ],
+  getInitialState: function() {
+    return {
+      items: {}
+    }
+  },
   componentWillMount: function() {
-    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    fb = new Firebase(rootUrl + 'items/');
+    this.bindAsObject(fb, 'items');
+    fb.on('value', this.handleDataLoaded);
   },
   render: function() {
     return <div className="row panel panel-default">
@@ -15,8 +24,15 @@ var Hello = React.createClass({
         <h2 className="text-center">
           To-Do List
         </h2>
+        <Header itemsStore={this.firebaseRefs.items}/>
+        <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+        <List items={this.state.items} />
+        </div>
       </div>
     </div>
+  },
+  handleDataLoaded: function() {
+    this.setState({loaded: true});
   }
 });
 
--- src/header.jsx
+++ src/header.jsx
@@ -1,14 +1,36 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
   render: function() {
     return <div className="input-group">
-      <input type="text" className="form-control" />
+      <input
+        value={this.state.text}
+        onChange={this.handleInputChange}
+        type="text"
+        className="form-control" />
       <span className="input-group-btn">
-        <button className="btn btn-default" type="button">
+        <button
+          onClick={this.handleClick}
+          className="btn btn-default"
+          type="button">
           Add
         </button>
       </span>
     </div>
+  },
+  handleClick: function() {
+    this.props.itemsStore.push({
+      text: this.state.text,
+      done: false
+    });
+    this.setState({text: ''});
+  },
+  handleInputChange: function(event) {
+    this.setState({text: event.target.value});
   }
-})
+});
